0e6f3c3bfac159f19db1e71c8ef100688d9e01d5
Yuki Aihara <aiharayuki0702@gmail.com>
2015-08-24 11:53:28 +0000

    delete files

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-    <div class="animsition wave">
-        <h1><img src="<?php echo get_template_directory_uri(); ?>/image/suchmos-logo.png" alt="Suchmos"></h1>
-    </div>
-<?php get_footer(); ?>
